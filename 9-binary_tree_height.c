d4286e32c875e4ee2f5a78e10043e1173a7bfe0e
Donald <donumeh54@gmail.com>
2024-06-05 12:54:37 +0100

    checks the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,18 +12,28 @@ size_t find_height(const binary_tree_t *);
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return find_height(tree) - 1;
+	if (tree == NULL)
+		return (0);
+
+	return (find_height(tree) - 1);
 }
 
 
 
+/**
+ * find_height - finds the height of a binary tree
+ * @tree: the head of the tree
+ *
+ * Return: the height of the tree
+ */
 
 size_t find_height(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
+
 	if (tree == NULL)
 		return (0);
-	
+
 	left = find_height(tree->left);
 	right = find_height(tree->right);
 
